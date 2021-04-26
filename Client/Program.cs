ce7bd49e3e5b4e4a15d547b3ff2633bad9ff52eb
Isoheptane <1374760231@qq.com>
2021-04-26 23:33:48 +0800

    Add: Created Client Project

---
+++ Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using Carbonate.Standard;
+using static Carbonate.Standard.ScreenIO;
+
+namespace Carbonate.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
