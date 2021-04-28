5d0af8a29068015262412f2952a7437ad919b01d
Isoheptane <1374760231@qq.com>
2021-04-28 12:52:39 +0800

    Change: Manual

--- Server/UserBehavior.Manual.cs
+++ Server/UserBehavior.Manual.cs
@@ -20,15 +20,27 @@ namespace Carbonate.Server
             if (arguments.Count == 0)
             {
                 string[] filePath = Directory.GetFiles($"{WorkspaceDirectory}/manual/");
-                string[] dirPath = Directory.GetDirectories($"{WorkspaceDirectory}/manual/");
                 StringBuilder message = new StringBuilder();
                 message.Append($"\n\\erThrere are \\cr{filePath.Length} \\erfiles in the manual folder.\n");
                 foreach (string path in filePath)
                     message.Append($"\\7r{Unescape(path)}\n");
-                message.Append($"\n\\erThrere are \\cr{dirPath.Length} \\erdirectories in the manual directory.\n");
-                foreach (string path in dirPath)
-                    message.Append($"\\7r{Unescape(path)}\n");
                 ServerMessage("server", user, message.ToString());
+            }
+            else
+            {
+                string targetPath = arguments[0];
+                string actualPath = $"{WorkspaceDirectory}/manual/{targetPath}";
+                if (File.Exists(actualPath))
+                {
+                    StringBuilder message = new StringBuilder();
+                    message.Append($"\n\\erFile \\cr\"{targetPath}\":\n");
+                    message.Append(File.ReadAllText(actualPath));
+                    ServerMessage("server", user, message.ToString());
+                }
+                else
+                {
+                    ServerMessage("server", user, $"\\crFile \"{targetPath}\" does not exist.");
+                }
             }
         }
     }
